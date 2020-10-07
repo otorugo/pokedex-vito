db96de6872c1faebc976dab625e8e321ab5a19ca
victorhugol <victorsilva698@usp.br>
2020-10-07 20:07:43 -0300

    View Pokedex adicionada

---
+++ src/view/Pokedex.js
@@ -0,0 +1,20 @@
+import Card from '../components/pokemon/Card.js';
+
+
+const Pokedex = {
+    name :'pokedex',
+    template : `
+        <div id="pokedex">
+            Pokedex aqui
+            <l1>
+                <card/>
+            </li>
+        </div>
+
+    `,
+    components : {
+        Card
+    },
+}
+
+export default Pokedex;
