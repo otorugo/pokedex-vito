97418b63c7e1fba1a83985f5d66f27e04ffe71cb
victorhugol <victorsilva698@usp.br>
2020-10-07 20:08:35 -0300

    Component para cada pokemon crido

---
+++ src/components/pokemon/Card.js
@@ -0,0 +1,17 @@
+const Card = {
+    name : 'card',
+    template : `
+        <div>
+            <img></img>
+            <div>
+                <p>nome</p>
+                <p>numero</p>
+                <p>tipo</p>
+            </div>
+        </div>
+    
+    `,
+}
+
+
+export default Card;
