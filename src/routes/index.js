fd98f31034f6f27dfa2fde3ed7021b931e700358
victorhugol <victorsilva698@usp.br>
2020-10-07 20:08:59 -0300

    Arquivo de info deletado

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,18 @@
 import Vue from '../lib/vue.js';
 import VueRouter from '../lib/vue-router.js';
+import Pokedex from '../view/Pokedex.js';
+
+
 
 Vue.use(VueRouter);
 
 
 const routes = [
-
+    {
+        path : '/',
+        name : 'pokedex',
+        component : Pokedex
+    },
 ]
 
 const router = new VueRouter({
