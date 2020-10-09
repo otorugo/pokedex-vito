1e0542328acf21b35656b0ae6776bd60811fb1ec
victorhugol <victorsilva698@usp.br>
2020-10-08 22:12:17 -0300

    server rodando

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 // const path = require('path');
 
+const PORT = process.env.PORT || 2020
+
+
 app.use(express.static('./docs'));
 
 app.listen(2020);
